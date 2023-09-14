848c8dbed65454b9d5711ed36a097d267e9fa4ed
Edgar Fernandez <efg_2001@hotmail.com>
2023-09-13 22:11:13 -0600

    James Bond Program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string name;
+        string last_name;
+        
+        //Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        last_name = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
     }
 }
