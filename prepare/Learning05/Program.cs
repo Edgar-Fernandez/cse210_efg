518f515e1552a78dfb1b77df452d68c53fbff9a6
Edgar Fernandez <efg_2001@hotmail.com>
2023-11-09 17:07:02 -0600

    W09 Learning - Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -22,6 +22,24 @@ class Program
         Console.WriteLine($"The cirle color is: {circle.GetColor()}");
         Console.WriteLine($"The circle area is: {circle.GetArea()}\n");
 
+        List<Shape> shapeList = new List<Shape>();
 
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        Console.WriteLine("\nList of shapes: \n");
+        //int i = 1;
+
+        foreach (Shape sh in shapeList)
+        {
+            Console.WriteLine($"The {sh.GetColor()} shape area is {sh.GetArea()} \n"); // I changed the output after comparing my program to the example solution
+            
+            //Console.WriteLine($"The shape {i} color is {sh.GetColor()}");
+            //Console.WriteLine($"The shape {i} area is {sh.GetArea()}\n");
+            //i++;
+        }
+
+        Console.WriteLine();
     }
 }
