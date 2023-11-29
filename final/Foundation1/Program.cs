3e33071881c5458abc94e29bfa454cd09eeb5d70
Edgar Fernandez <efg_2001@hotmail.com>
2023-11-28 22:40:44 -0600

    W12 Foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -9,32 +9,36 @@ class Program
 
     List<Video> videos = new List<Video>(); // We create an empty list of Video
 
-    Video video1 = new Video(); // Create 1rst Video with no comments
-    video1._title = "Cute cat";
-    video1._author = "Jonh Smith";
-    video1._length = 35;
+    Video newVideo = new Video(); // Create 1rst Video with no comments
+    newVideo._title = "Cute cat";
+    newVideo._author = "Jonh Smith";
+    newVideo._length = 35;
     
-    Comment comment1 = new Comment (); // Create 1rst comment for video
-    comment1._name = "Paolo";
-    comment1._comment = "The video was great!";
-    video1.AddComment(comment1); // Add it to video
+    Comment newComment = new Comment (); // Create 1rst comment for video
+    newComment._name = "Paolo";
+    newComment._comment = "The video was great!";
+    newVideo.AddComment(newComment); // Add it to video
 
     Comment comment2 = new Comment (); // Create 2nd comment for video
     comment2._name = "Gabriela";
     comment2._comment = "I really liked the cats!";
-    video1.AddComment(comment2); // Add it to video
+    newVideo.AddComment(comment2); // Add it to video
 
     Comment comment3 = new Comment (); // Create 3rd comment for video
     comment3._name = "Juan Luis";
     comment3._comment = "I have seen other videos like this";
-    video1.AddComment(comment3); // Add it to video
+    newVideo.AddComment(comment3); // Add it to video
 
     Comment comment4 = new Comment (); // Create 4th comment for video
     comment4._name = "Jose";
     comment4._comment = "Nice video!";
-    video1.AddComment(comment4); // Add it to video
+    newVideo.AddComment(comment4); // Add it to video
 
-    videos.Add(video1);
+    videos.Add(newVideo);
+
+    //********************************************
+
+    
 
     Console.Clear();
     
