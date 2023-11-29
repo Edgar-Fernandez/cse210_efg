7efe6fe25636da94f6042bbd2915bcf35a406642
Edgar Fernandez <efg_2001@hotmail.com>
2023-11-28 22:35:04 -0600

    W12 Foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,20 +7,41 @@ class Program
         //Console.WriteLine("Hello Foundation1 World!");
         //Program 1: Abstraction with Youtube Videos
 
+    List<Video> videos = new List<Video>(); // We create an empty list of Video
+
     Video video1 = new Video(); // Create 1rst Video with no comments
     video1._title = "Cute cat";
     video1._author = "Jonh Smith";
-    video1.length = 35;
+    video1._length = 35;
     
-    Comment comment1 = new Comment (); // Create 1rst comment
+    Comment comment1 = new Comment (); // Create 1rst comment for video
     comment1._name = "Paolo";
     comment1._comment = "The video was great!";
     video1.AddComment(comment1); // Add it to video
 
-    Comment comment2 = new Comment (); // Create 2nd comment
+    Comment comment2 = new Comment (); // Create 2nd comment for video
     comment2._name = "Gabriela";
     comment2._comment = "I really liked the cats!";
     video1.AddComment(comment2); // Add it to video
+
+    Comment comment3 = new Comment (); // Create 3rd comment for video
+    comment3._name = "Juan Luis";
+    comment3._comment = "I have seen other videos like this";
+    video1.AddComment(comment3); // Add it to video
+
+    Comment comment4 = new Comment (); // Create 4th comment for video
+    comment4._name = "Jose";
+    comment4._comment = "Nice video!";
+    video1.AddComment(comment4); // Add it to video
+
+    videos.Add(video1);
+
+    Console.Clear();
+    
+    foreach (Video video in videos)
+    {
+        video.DisplayVideoData(video.CommentsNumber());
+    }
 
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,7 +3,7 @@ public class Video
     //Attributes
     public string _title;
     public string _author;
-    public int length;
+    public int _length;
     public  List<Comment> _comments = new List<Comment>();
 
     //Methods
@@ -16,11 +16,20 @@ public class Video
 
     public int CommentsNumber()
     {
-        return 0;
+        return _comments.Count();
     }
 
-    public void DisplayVideoData()
+    public void DisplayVideoData(int comments)
     {
+        int position = 1;
+        Console.WriteLine($"Video \"{_title}\" by {_author} has a length of {_length} seconds");
+        Console.WriteLine($"and it received the following {comments} comments: \n");
+        
+        foreach (Comment commentDisplayed in _comments)
+        {
+            Console.WriteLine($"{position}. {commentDisplayed._name}: \"{commentDisplayed._comment}\" ");
+            position++;
+        }
         return;
     }
 
