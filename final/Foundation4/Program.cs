df9e70686bc3a23ac7c5fea5a35b3fb173061734
Edgar Fernandez <efg_2001@hotmail.com>
2023-12-07 21:50:46 -0600

    W13 Foundation Programs Completed

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Foundation4 World!");
-        //This program created different Exercise Activities
+        //This program createS different Exercise Activities
         RunningActivity r1 = new RunningActivity("01 Jan 2023", 30, 10);
         CyclingActivity c1 = new CyclingActivity("14 Feb 2023", 60, 30);
         SwimmingActivity s1 = new SwimmingActivity("25 Dec 2023", 20, 20);
@@ -17,7 +17,7 @@ class Program
 
         Console.Clear();
         Console.WriteLine("\n\t>>>>>  LIST OF ACTIVITIES  <<<<<\n");
-        
+
         int i = 1;
         foreach (Activity activity in activities)
         {
@@ -25,16 +25,16 @@ class Program
             i++;
         }
 
+        /******************************************CALIBRATIG DATA***********************************************
+        To get an example of the classes calculating Distance, Speed, and Pace,
+        uncomment the following 7 lines of code that use 30 minutes as Length (time):              */
 
-        /**************************CALIBRATIG DATA****************************
-        To get an example of the classes calculating Dictance, Speed and Pace,
-        uncomment the following 6 lines of code that use 30 minutes as Length (time):   */
-        /*RunningActivity r1 = new RunningActivity("03 Nov 2022", 30, 4.8); //4.8 km is equal to 96 laps (50m each lap)
-        Console.WriteLine(r1.GetSummary());
-        CyclingActivity c1 = new CyclingActivity("03 Nov 2022", 30, 9.6); //9.6 km/h during 30 minutes gives a distance of 4.8km
-        Console.WriteLine(c1.GetSummary());
-        SwimmingActivity s1 = new SwimmingActivity("03 Nov 2022", 30, 96); //96 laps is equal to 4.8km or 4800m
-        Console.WriteLine(s1.GetSummary());*/
-
+        /*Console.WriteLine("\nACTIVITIES CREATED USING DATA FROM COURSE EXAMPLE: \n");
+        RunningActivity r1t = new RunningActivity("03 Nov 2022", 30, 4.8); //4.8 km is equal to 96 laps (50m each lap)
+        Console.WriteLine(r1t.GetSummary());
+        CyclingActivity c1t = new CyclingActivity("03 Nov 2022", 30, 9.6); //9.6 km/h during 30 minutes gives a distance of 4.8km
+        Console.WriteLine(c1t.GetSummary());
+        SwimmingActivity s1t = new SwimmingActivity("03 Nov 2022", 30, 96); //96 laps is equal to 4.8km or 4800m
+        Console.WriteLine(s1t.GetSummary());*/
     }
 }
